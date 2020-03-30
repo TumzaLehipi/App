8a833cd30b06d2842743707176714d2692891b1f
tumzalehipi <brianmoipolai1@gmail.com>
2020-03-30 16:33:55 +0200

    styling and css basics

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -21,12 +21,12 @@ export default function NameList() {
             age: 30,
             skill: 'react'
         },
-]
-    const nameList = names.map(name => <h2>{name}</h2>)
+    ]
+    const nameList = names.map((name, index) => <h2 key={index}>{index}{name}</h2>)
     return (
         <div>
             {
-                names.map(name => <h2>{name}</h2>)
+                nameList
             }
         </div>
 
