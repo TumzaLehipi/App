7e9f70ff3e436484923c9cf070af0f99a9131d45
tumzalehipi <brianmoipolai1@gmail.com>
2020-03-30 14:40:43 +0200

    namelist

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,11 +2,33 @@ import React from 'react'
 
 export default function NameList() {
     const names = ['tumza', 'rtgw', 'werfe']
+    const person = [
+        {
+            id: 1,
+            name: 'tumza',
+            age: 30,
+            skill: 'react'
+        },
+        {
+            id: 2,
+            name: 'rtgw',
+            age: 30,
+            skill: 'react'
+        },
+        {
+            id: 3,
+            name: 'werfe',
+            age: 30,
+            skill: 'react'
+        },
+]
+    const nameList = names.map(name => <h2>{name}</h2>)
     return (
         <div>
             {
                 names.map(name => <h2>{name}</h2>)
             }
         </div>
+
     )
 }
