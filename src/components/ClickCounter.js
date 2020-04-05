f3795f0446b7b1e1b0397cc05bda1d0042929f62
tumzalehipi <brianmoipolai1@gmail.com>
2020-04-05 23:12:53 +0200

    Reuse methods

---
+++ src/components/ClickCounter.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import UpdatedCompoment from './WithCounter'
+import WithCounter from './WithCounter'
+
+
+class ClickCounter extends Component {
+
+    render() {
+        const { count, incrementCount } = this.props
+        return (
+
+            <button onClick={incrementCount}> 
+            {this.props.name} Clicked {count} times
+            </button>
+
+        )
+    }
+}
+
+export default WithCounter(ClickCounter, 5) 
---
+++ src/components/ClickCounterTwo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ClickCounterTwo extends Component {
+ 
+    render() {
+        const {count, incrementCount } = this.props
+        return (
+           <button onClick = {incrementCount}>Cliked {count} times</button>
+        )
+    }
+}
+
+export default ClickCounterTwo
